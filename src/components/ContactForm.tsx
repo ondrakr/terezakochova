35a19d47cb3755037df2b7f687dc11ba68ecf10b
ondrakr <ondikrejci83@gmail.com>
2025-04-16 11:10:02 +0200

    o

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -31,7 +31,7 @@ const ContactForm = () => {
 
                 <div className="flex items-center gap-4">
                   <div className="flex items-center justify-center">
-                    <img src="/Whatsapp2_icon.png" alt="WhatsApp" className="w-10 xl:w-12 h-10 xl:h-12" />
+                    <img src="/Ig_icon.png" alt="WhatsApp" className="w-10 xl:w-12 h-10 xl:h-12" />
                   </div>
                   <a href="https://wa.me/420603871299" target="_blank" rel="noopener noreferrer" className="text-base leading-[25px] text-gray-700 hover:text-[#8EA37F] transition-colors">
                     Whatsapp
